d43a25dcc2215cecf04b5346b4d2fe85aff48a2d
senbet22 <senbet22@users.noreply.github.com>
2026-03-21 17:19:46 +0100

    solution to task 1: height of triangle.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+/* 
+Input: single integer n (height of the triangle)
+Example: n = 5
+*
+**
+***
+****
+***** 
+*/
+
+Console.WriteLine("Please enter the height of the desired triangle.");
+Console.WriteLine("example: 5");
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine(new string('*', i));
+}
+
+Console.ReadKey();
