c0583db0e72f609351178ad94f73c9171cd35040
senbet22 <senbet22@users.noreply.github.com>
2026-03-22 15:50:14 +0100

    Completed task 1 and 2. Folder re-structure

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Input: single integer n (height)
+Example: n = 6
+1
+22
+333
+4444
+55555
+666666*/
+
+Console.WriteLine("Please enter the height of the desired triangle.");
+Console.WriteLine("example: 5");
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+  for (int j = 1; j <= i; j++)
+  {
+    Console.Write(i);
+  }
+  Console.WriteLine();
+}
