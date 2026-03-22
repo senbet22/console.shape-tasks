c99acb6aa062fe2dcb2903d190df36b9d78aad13
senbet22 <senbet22@users.noreply.github.com>
2026-03-22 18:11:52 +0100

    Refactoring of Task1.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -16,5 +16,9 @@ int n = int.Parse(Console.ReadLine());
 
 for (int i = 1; i <= n; i++)
 {
-    Console.WriteLine(new string('*', i));
+  for (int j =1; j <= i; j++)
+  {
+    Console.Write("*");
+  }
+  Console.WriteLine();
 }
